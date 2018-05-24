ee1cb933a2af7ad6f62a2cef613fc983c2f7dbb5
alex <zhurbitsky@gmail.com>
2018-05-24 18:59:44 +0300

    Test Rainbow HAT

--- app/src/main/java/com/example/androidthings/weatherstation/WeatherStationActivity.java
+++ app/src/main/java/com/example/androidthings/weatherstation/WeatherStationActivity.java
@@ -17,21 +17,53 @@
 package com.example.androidthings.weatherstation;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import com.google.android.things.contrib.driver.apa102.Apa102;
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class WeatherStationActivity extends Activity {
     private static final String TAG = WeatherStationActivity.class.getSimpleName();
 
     // Default LED brightness
     private static final int LEDSTRIP_BRIGHTNESS = 1;
+    private AlphanumericDisplay mDisplay;
+    private Apa102 mLedstrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Weather Station Started");
 
-        //TODO: Register peripheral drivers here
+        // Initialize 14-segment display
+        try {
+            mDisplay = RainbowHat.openDisplay();
+            mDisplay.setEnabled(true);
+            mDisplay.display("1234");
+            Log.d(TAG, "Initialized I2C Display");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing display", e);
+        }
+
+        // Initialize LED strip
+        try {
+            mLedstrip = RainbowHat.openLedStrip();
+            mLedstrip.setBrightness(LEDSTRIP_BRIGHTNESS);
+            int[] colors = new int[7];
+            Arrays.fill(colors, Color.RED);
+            mLedstrip.write(colors);
+            // Because of a known APA102 issue, write the initial value twice.
+            mLedstrip.write(colors);
+
+            Log.d(TAG, "Initialized SPI LED strip");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing LED strip", e);
+        }
     }
 
     @Override
@@ -52,7 +84,29 @@ public class WeatherStationActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
 
-        //TODO: Close peripheral connections here
+        if (mDisplay != null) {
+            try {
+                mDisplay.clear();
+                mDisplay.setEnabled(false);
+                mDisplay.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing display", e);
+            } finally {
+                mDisplay = null;
+            }
+        }
+
+        if (mLedstrip != null) {
+            try {
+                mLedstrip.setBrightness(0);
+                mLedstrip.write(new int[7]);
+                mLedstrip.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing LED strip", e);
+            } finally {
+                mLedstrip = null;
+            }
+        }
     }
 
     /**
